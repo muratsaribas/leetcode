d10e695e6485756c04e0876291267c7a56329aa9
Murat Sarıbaş <81035521+muratsaribas@users.noreply.github.com>
2022-09-02 17:38:17 +0300

    Time: 8 ms (48.85%), Space: 47.4 MB (51.07%) - LeetHub

---
+++ 36-valid-sudoku/36-valid-sudoku.java
@@ -0,0 +1,51 @@
+class Solution {
+    public boolean isValidSudoku(char[][] board) {
+        
+        for(int i=0; i<board.length; i++){
+            Set<Character> row = new HashSet<>();
+            for(int j=0; j<board.length; j++){
+                if(board[i][j] == '.'){
+                    continue;
+                }
+                if(row.contains(board[i][j])){
+                    return false;
+                }
+                row.add(board[i][j]);
+            }
+        }
+        
+        for(int i=0; i<board.length; i++){
+            Set<Character> col = new HashSet<>();
+            for(int j=0; j<board.length; j++){
+                if(board[j][i] == '.'){
+                    continue;
+                }
+                
+                if(col.contains(board[j][i])){
+                    return false;
+                }
+                col.add(board[j][i]);
+            }
+        }
+        
+        for (int i = 0; i < board.length; i = i + 3) {
+            for (int j = 0; j < board.length; j = j + 3) {
+                Set<Character> charSet = new HashSet<>();
+                for (int l = i, count = 0; count < 3; count++, l++) {
+                    for (int k = j, cnt = 0; cnt < 3; cnt++, k++) {
+                        if (board[l][k] == '.') {
+                            continue;
+                        }
+                        if (charSet.contains(board[l][k])) {
+                            return false;
+                        }
+                        charSet.add(board[l][k]);
+                    }
+                }
+
+            }
+        }
+        
+        return true;
+    }
+}
