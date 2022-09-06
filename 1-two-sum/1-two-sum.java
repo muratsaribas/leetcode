695202b6373571398ad3dd68fb14710d8532f994
Murat Sarıbaş <81035521+muratsaribas@users.noreply.github.com>
2022-09-06 09:56:58 +0300

    Time: 83 ms (31.05%), Space: 45.1 MB (60.25%) - LeetHub

--- 1-two-sum/1-two-sum.java
+++ 1-two-sum/1-two-sum.java
@@ -1,14 +1,21 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        for(int i=0; i<nums.length-1; i++){
+        int[] res = {0,0};
+        boolean flag = false;
+        for(int i=0; i<nums.length; i++){
             for(int j=i+1; j<nums.length; j++){
-                if(nums[i] + nums[j] == target){
-                    return new int[]{i ,j};
+                int sum = nums[i] + nums[j];
+                if(sum == target){
+                    res[0] = i;
+                    res[1] = j;
+                    flag = true;
+                    break;
                 }
             }
+            if(flag) break;
         }
         
-        return null;
+        return res;
         
     }
 }
