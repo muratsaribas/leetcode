764856f9fa53ad81f0411923c2caef66e271cbf4
Murat Sarıbaş <81035521+muratsaribas@users.noreply.github.com>
2022-09-11 19:36:29 +0300

    Time: 22 ms (6.07%), Space: 50.1 MB (53.44%) - LeetHub

--- 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.java
+++ 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.java
@@ -1,39 +1,26 @@
 class Solution {
-    
-    int binarySearch(int array[], int element, int low, int high) {
-
-        while (low <= high) {
-
-          int mid = low + (high - low) / 2;
-
-          if (array[mid] == element)
-            return mid;
-
-          if (array[mid] < element)
-            low = mid + 1;
-
-          else
-            high = mid - 1;
-        }
-
-        return -1;
-    }
-    
+        
     public int[] twoSum(int[] numbers, int target) {
         
-        int[] res = new int[2];
-        
         for(int i=0; i<numbers.length; i++){
-            int newTarget = numbers[i] > 0 ? target-numbers[i] : target+Math.abs(numbers[i]);
-            int other = binarySearch(numbers, newTarget, i+1, numbers.length-1);
-            if(other != -1){
-                res[0] = i+1;
-                res[1] = other+1;
-                return res;
+            int low = i + 1;
+            int high = numbers.length - 1;
+            while (low <= high) {
+
+              int mid = low + (high - low) / 2;
+
+              if (numbers[mid] == target - numbers[i])
+                return new int[]{i+1, mid+1};
+
+              else if (numbers[mid] > target - numbers[i])
+                high = mid - 1;
+
+              else
+                low = mid + 1;
             }
         }
         
-        return res;
+        return new int[]{-1,-1};
         
     }
 }
