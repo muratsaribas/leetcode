200cb8183198d78d5aaf6999a42ff0374312ff1c
Murat Sarıbaş <81035521+muratsaribas@users.noreply.github.com>
2022-09-17 17:59:44 +0300

    Time: 0 ms (100.00%), Space: 41.4 MB (75.88%) - LeetHub

--- 1491-average-salary-excluding-the-minimum-and-maximum-salary/1491-average-salary-excluding-the-minimum-and-maximum-salary.java
+++ 1491-average-salary-excluding-the-minimum-and-maximum-salary/1491-average-salary-excluding-the-minimum-and-maximum-salary.java
@@ -1,12 +1,15 @@
 class Solution {
     public double average(int[] salary) {
+        
         double min = salary[0];
         double max = salary[0];
         double sum = salary[0];
         
         for(int i = 1; i<salary.length; i++){
             if(salary[i] < min) min = salary[i];
+            
             else if(salary[i] > max) max = salary[i];
+            
             sum += salary[i];
         }
         
